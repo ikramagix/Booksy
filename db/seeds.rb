89b84326aaf1ac49bbb486ea0fc6f51054f81e96
ikramiste <ikrame.saadi@gmail.com>
2024-03-04 12:18:14 +0200

    first commit - articles + seed + basic bootstrap

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+
+30.times do
+    Article.create!(
+      title: Faker::Book.title,
+      content: Faker::Lorem.paragraph(sentence_count: 5)
+    )
+  end
+  
